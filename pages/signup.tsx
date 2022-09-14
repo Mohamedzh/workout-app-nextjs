8a302ceb2f0371f99a0d2962ec109bd8e0edde9b
Mohamed Zahran <mohamed.zahran1@live.com>
2022-09-14 19:00:08 +0200

    fixed signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -60,7 +60,8 @@ const signup: NextPage = (props: Props) => {
             <div className='mt-6'>
               <form action='#' method='POST' className='space-y-6'>
                 <div>
-                  <div>
+                  <div className="flex space-x-3">
+                    <div>
                     <label
                       htmlFor='email'
                       className='block text-sm font-medium text-gray-700'
@@ -79,6 +80,7 @@ const signup: NextPage = (props: Props) => {
                         required
                         className='block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                       />
+                    </div>
                     </div>
                     <div className=''>
                       <label
